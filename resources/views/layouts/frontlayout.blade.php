bcddeb38d53e21c3b9fc2ddf7843b7360b9dfdea
Anthony <anthony.fabregat@gmail.com>
2018-02-09 10:30:48 +0100

    pushAnthony

---
+++ resources/views/layouts/frontlayout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/front-style.css') }}" />
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+    <div id="wrapper">
+      <header>
+        <h1> holistwood </h1>
+      </header>
+
+      <main>
+
+          <div id="trailer">
+            <iframe src="https://www.youtube.com/embed/a8v__0kHzNg" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          </div>
+
+          <div class="affiches">
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+            <article class="affiche"> </article>
+          </div>
+
+      </main>
+
+      <footer>
+
+     </footer>
+   </div>
+ </body>
+</html>
