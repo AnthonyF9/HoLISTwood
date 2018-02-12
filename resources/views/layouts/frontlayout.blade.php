3048b92c285fd557296de8bf29588532db62d85a
alison <alison.couroyer@gmail.com>
2018-02-12 14:13:36 +0100

    alison

--- resources/views/layouts/frontlayout.blade.php
+++ resources/views/layouts/frontlayout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/slicknav.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/front-style.css') }}" />
     <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
@@ -12,9 +13,34 @@
   <body>
     <div id="wrapper">
       <header>
+        <nav class="js">
+          <ul class="slicknav_menu">
+            <li><a class="@yield('activehome')" href="{{ route('home') }}">Accueil</a></li>
+            @guest
+            <li><a class="@yield('activelogin')" href="{{ route('login') }}">Se connecter</a></li>
+            <li><a class="@yield('activeregister')" href="{{ route('register') }}">S'inscrire</a></li>
+            @else
+              @if ( Auth::user()->role == 'admin')
+            <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+              @endif
+            <li><a class="@yield('activeprofile')" href="{{ route('profile') }}">{{ Auth::user()->name }}</a></li>
+            <li id="hello-user">
+                   <div id="logout"><a href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                       Se déconnecter
+                     </a>
+                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                         {{ csrf_field() }}
+                     </form>
+                   </div>
+                 </li>
+            @endguest
+          </ul>
+        </nav>
         <h1> holistwood </h1>
-        <nav>
-          <ul>
+        <nav class="js">
+          <ul id="menu">
             <li><a class="@yield('activehome')" href="{{ route('home') }}">Accueil</a></li>
             @guest
             <li><a class="@yield('activelogin')" href="{{ route('login') }}">Se connecter</a></li>
@@ -52,6 +78,8 @@
      </footer>
      <script type="text/javascript" src="{{ asset('js/jquery-3.2.1.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/jquery-ui.js') }}"></script>
+     <script type="text/javascript" src="{{ asset('js/jquery.slicknav.min.js') }}"></script>
+     <script type="text/javascript" src="{{ asset('js/burger.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
    </div>
  </body>
