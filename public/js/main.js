14361b7c6b6e2ab64851436f9257e3968b08afc0
Guillaume Giudici <gui.giudici@gmail.com>
2018-02-12 14:11:30 +0100

    gui

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,28 @@ $(document).ready(function() {
   $('input[type="submit"]').on('mouseover',function () {
     $(this).css({'cursor':'pointer'})
   });
-  // $('#search-by-id-button').click(function () {
-  //     var c = $("#search-by-id-form #i").filter(function (index, element) {
-  //         return $(element).val() != "";
-  //     }).serialize();
-  //     console.log(c);
-  //     var d = 'http://www.omdbapi.com/?' + c + '&apikey=67f441ca';
-  //     console.log(d);
-  // });
+
+  //////////////////////////////////////////////////
+  ///////////// La modale de Connexion /////////////
+  //////////////////////////////////////////////////
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
--- resources/views/layouts/frontlayout.blade.php
+++ resources/views/layouts/frontlayout.blade.php
@@ -17,7 +17,60 @@
           <ul>
             <li><a class="@yield('activehome')" href="{{ route('home') }}">Accueil</a></li>
             @guest
-            <li><a class="@yield('activelogin')" href="{{ route('login') }}">Se connecter</a></li>
+            <li><button id="myBtn" type="button" name="button">Se connecter</button></li>
+            {{-- <li><a id="myBtn" class="@yield('activelogin')" href="{{ route('login') }}">Se connecter</a></li> --}}
+            <div id="myModal" class="modal">
+              <div class="modal-content">
+                <div class="panel panel-default">
+                  <span class="close">&times;</span>
+                  <div class="panel-heading">Login</div>
+                  <div class="panel-body">
+                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                         {{ csrf_field() }}
+                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                             <div class="col-md-6">
+                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                 @if ($errors->has('email'))
+                                     <span class="help-block">
+                                         <strong>{{ $errors->first('email') }}</strong>
+                                     </span>
+                                 @endif
+                             </div>
+                         </div>
+                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                             <label for="password" class="col-md-4 control-label">Password</label>
+                             <div class="col-md-6">
+                                 <input id="password" type="password" class="form-control" name="password" required>
+                                 @if ($errors->has('password'))
+                                     <span class="help-block">
+                                         <strong>{{ $errors->first('password') }}</strong>
+                                     </span>
+                                 @endif
+                             </div>
+                         </div>
+                         <div class="form-group">
+                             <div class="col-md-6 col-md-offset-4">
+                                 <div class="checkbox">
+                                     <label>
+                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                     </label>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="form-group">
+                             <div class="col-md-8 col-md-offset-4">
+                                 <button type="submit" class="btn btn-primary">Login</button>
+                                 <a class="btn btn-link" href="{{ route('password.request') }}">Forgot Your Password?</a>
+                             </div>
+                         </div>
+                     </form>
+                  </div>
+                </div>
+             </div>
+            </div>
+
+
             <li><a class="@yield('activeregister')" href="{{ route('register') }}">S'inscrire</a></li>
             @else
               @if ( Auth::user()->role == 'admin')
