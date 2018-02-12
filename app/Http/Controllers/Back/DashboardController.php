2116e4d03839d19d091e5dc6ceeb298d375b6ca8
Anthony <anthony.fabregat@gmail.com>
2018-02-12 10:02:52 +0100

    'anthony'

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -12,4 +12,25 @@ class DashboardController extends Controller
   {
       return view('layouts/backlayout');
   }
+  
+  protected function addmovie(array $imdb)
+  {
+      return Movies::create([
+          'title' => $imdb['name'],
+          'year' => $imdb['year'],
+          'runtime' => $imdb['runtime'],
+          'director' => $imdb['director'],
+          'writers' => $imdb['writers'],
+          'actors' => $imdb['actors'],
+          'plot' => $imdb['plot'],
+          'awards' => $imdb['awards'],
+          'imdp_ip' => $imdb['imdp_ip'],
+          'production' => $imdb['production'],
+          'website' => $imdb['website'],
+          'genre' => $imdb['genre'],
+          'status' => $imdb['status'],
+          'created_at' => $imdb['created_at'],
+          'updated_at' => $imdb['updated_at'],
+      ]);
+  }
 }
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -13,7 +13,23 @@ active @endsection
       <iframe width = "917px" height="490px" src="https://www.youtube.com/embed/a8v__0kHzNg" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
     </div>
   </div>
+<?php
+  $opts = array(
+    'http' => array(
+        'method' => "GET"
+    )
+  );
 
+  $context = stream_context_create($opts);
+  $url = 'http://www.omdbapi.com/?t=return&y=2003&apikey=224b73f2';
+
+  $raw = file_get_contents($url, true, $context);
+  $json = json_decode($raw, true);
+
+  echo '<pre>';
+  print_r($json);
+  echo '</pre>';
+?>
 
   <div class="affiches">
     <article class="affiche"> </article>
