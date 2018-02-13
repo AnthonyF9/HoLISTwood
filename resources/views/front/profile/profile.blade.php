5b9ff9d7d1f3686e2a9ab517365ab7f906ecffae
alison <alison.couroyer@gmail.com>
2018-02-13 16:52:04 +0100

    Alison

--- resources/views/front/profile/profile.blade.php
+++ resources/views/front/profile/profile.blade.php
@@ -8,5 +8,55 @@
 active @endsection
 
 @section('content')
-  Bonjour {{ Auth::user()->name }}
+  <div class="profile-head">
+    <img src="{{ asset('img/avatar.svg') }}" alt="avatar">
+    <h1>Pseudo</h1>
+    <!-- {{ Auth::user()->name }} -->
+  </div>
+  <nav class="profile-menu">
+    <ul>
+      <li><a href="#">Favorite Movie</a></li>
+      <li><a href="#">Movie Rate</a></li>
+      <li><a href="#">Setting</a></li>
+    </ul>
+  </nav>
+
+  <div class="profile-favorite">
+    <article class="affiche"> </article>
+    <article class="affiche"> </article>
+    <article class="affiche"> </article>
+  </div>
+
+  <div class="profile-rating">
+    <article class="affiche"> </article>
+    <article class="affiche"> </article>
+    <article class="affiche"> </article>
+  </div>
+
+  <div class="profile-setting">
+    <nav>
+      <ul>
+        <li><a href="#">Info</a></li>
+        <li><a href="#">Avatar</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Notification</a></li>
+      </ul>
+    </nav>
+
+    <div class="setting-info">
+      
+    </div>
+
+    <div class="setting-avatar">
+
+    </div>
+
+    <div class="setting-contact">
+
+    </div>
+
+    <div class="setting-notificaton">
+
+    </div>
+  </div>
 @endsection
