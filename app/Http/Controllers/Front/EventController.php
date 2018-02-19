cc41b9f6d3ae210ff5b8a8b9a84f548c9be97dde
AnthonyF9 <anthony.fabregat@gmail.com>
2018-02-19 21:11:42 +0100

    'anthony'

--- app/Http/Controllers/Front/EventController.php
+++ app/Http/Controllers/Front/EventController.php
@@ -6,25 +6,29 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Calendar;
 use App\Event;
+use App\Movie;
 
 class EventController extends Controller
 {
   public function index()
    {
+       $movies = \DB::table('movies')->where('release_date','!=','null')->get();
+       // dd($movies);
+
        $events = [];
        $data = Event::all();
        if($data->count()) {
-           foreach ($data as $key => $value) {
+           foreach ($movies as $key => $movie) {
                $events[] = Calendar::event(
-                   $value->title,
+                   $movie->title,
                    true,
-                   new \DateTime($value->start_date),
-                   new \DateTime($value->end_date.' +1 day'),
+                   new \DateTime($movie->release_date),
+                   new \DateTime($movie->release_date.' +1 day'),
                    null,
                    // Add color and link on event
                  [
                      'color' => '#3A87AD',
-                     'url' => 'pass here url and any route',
+                     'url' => '',
                  ]
                );
            }
