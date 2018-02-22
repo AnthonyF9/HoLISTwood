fff26d594cc35c510aebb8d499b4a01d0665bec7
Guillaume Giudici <gui.giudici@gmail.com>
2018-02-22 10:14:26 +0100

    gui

--- resources/views/front/oneMovie.blade.php
+++ resources/views/front/oneMovie.blade.php
@@ -155,9 +155,17 @@
       </div><!-- .comment-list -->
     @endif
     {{-- {{ dd($allcomments) }} --}}
+    {{-- {{ dd(session()) }} --}}
     @foreach ($allcomments as $key => $onecomment)
       <div class="comment-list">
-        <h4><span>{{ $onecomment->name }}</span> the {{ $onecomment->created_at }}</h4>
+        <div class="one-comment">
+          <h4><span>{{ $onecomment->name }}</span> the {{ $onecomment->created_at }}</h4>
+          {{-- @if (Auth::user())
+            @if ($condition)
+
+            @endif
+          @endif --}}
+        </div>
         <p>{{ $onecomment->content }}</p>
       </div><!-- .comment-list -->
     @endforeach
