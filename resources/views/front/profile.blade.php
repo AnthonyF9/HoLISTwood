f7834082b20510f673fbde31b135153e9e416501
Guillaume Giudici <gui.giudici@gmail.com>
2018-02-26 12:47:08 +0100

    gui

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -26,14 +26,44 @@ active @endsection
       <tr>
         <th>Title</th>
         <th>Status</th>
-        {{-- <th>My rate</th> --}}
+        <th>My rate</th>
       </tr>
+      {{-- {{ dd($mymoviesrating) }} --}}
       {{-- {{ dd($mymovieslist) }} --}}
+      @php
+        $ratingmovie = [];
+      @endphp
+      @foreach ($mymoviesrating as $onemovierating)
+        @php
+          $ratingmovie[] = $onemovierating->id_movie;
+        @endphp
+      @endforeach
+      {{-- {{ dd($ratingmovie) }} --}}
       @foreach ($mymovieslist as $movie)
         <tr>
           <td><a href="{{ route('oneMovieAuth', array( 'imdb_id'=> $movie->imdb_id )) }}">{{$movie->title}}</a></td>
           <td>{{$movie->statuslist}}</td>
-          {{-- <td>{{$movie->note}}</td> --}}
+          <td>
+            @foreach ($mymoviesrating as $onemovierating)
+              @if ($onemovierating->id_user == Auth::user()->id)
+                  {{-- {{ in_array($movie->movie_id,$ratingmovie) }} --}}
+                @if (in_array($movie->movie_id,$ratingmovie) != 0)
+                  @if ($onemovierating->id_movie == $movie->movie_id)
+                    @if (!empty($onemovierating->note))
+                      {{$onemovierating->note}} / 5
+                    @else
+                      @php $noraitingavailable = "No rating available"; @endphp
+                    @endif
+                  @endif
+                @else
+                  @php $noraitingavailable = "No rating available"; @endphp
+                @endif
+              @endif
+            @endforeach
+            @if (isset($noraitingavailable))
+              {{ $noraitingavailable }}
+            @endif
+          </td>
         </tr>
       @endforeach
     </table>
